52c5a7ab3586f18e04201c38823ca367a9da489b
Zhang Zijing <zhang_zi_jing@hotmail.com>
2016-08-16 23:48:02 +0800

    Update playurl.php

--- app/playurl.php
+++ app/playurl.php
@@ -1,1 +1,1 @@
-<?php echo file_get_contents("http://interface.bilibili.com/playurl?".urldecode( $_GET["argument"]););?>
+<?php echo file_get_contents("http://interface.bilibili.com/playurl?".urldecode( $_GET["argument"]));?>
