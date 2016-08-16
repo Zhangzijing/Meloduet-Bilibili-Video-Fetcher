6c5ed68f051a9af3ddb2d93d4214ae1e63c0d816
ZhangZijing <zhang_zi_jing@hotmail.com>
2016-08-16 20:39:42 +0800

    精简页面代理

--- app/playurl.php
+++ app/playurl.php
@@ -1,4 +1,1 @@
-<?php /*保证跨站请求API进行的二次代理, 可以使用第三方JSONP代理这样就可以不用搭建服务器.*/
-$url="http://interface.bilibili.com/playurl?".urldecode( $_GET["argument"]);
-echo file_get_contents($url);
-	?>
+<?php echo file_get_contents("http://interface.bilibili.com/playurl?".urldecode( $_GET["argument"]););?>
